69f15ffc191acb0766a95a897b376ba27366bccc
Aaly16 <aalycat16@gmail.com>
2021-07-26 16:18:58 -0600

    Added weather forecast JS

--- src/index.js
+++ src/index.js
@@ -22,6 +22,56 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let weatherForecast = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+<div class="col-2">
+  <div class="weekday">${formatDay(forecastDay.dt)}</div>
+     <img
+      src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png"
+      alt=""
+      width="40"
+      class="emojis"
+      />
+  <div class="temperature">
+ <span id="forecast-temperature-hi"> ${Math.round(
+   forecastDay.temp.max
+ )}° </span>
+<span id="forecast-temperature-low"> ${Math.round(
+          forecastDay.temp.min
+        )}° </span>
+      </div>
+      </div>
+`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  weatherForecast.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "2614fc6e3d33da70c2fc7c9bb343ecb7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   let tempNumber = document.querySelector("#temp-number");
   let city = document.querySelector("#city");
@@ -44,10 +94,12 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   mainEmoji.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function findCity(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "2614fc6e3d33da70c2fc7c9bb343ecb7";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
@@ -70,13 +122,11 @@ function getCurrentLocation(event) {
 }
 
 function calculatePosition(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "2614fc6e3d33da70c2fc7c9bb343ecb7";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemperature);
 }
-
-let celsiusTemp = null;
 
 findCity("Edmonton");
